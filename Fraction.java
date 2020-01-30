10b48f5f66967b1a6db0cb13b6ed725d3a730af5
Chris Nelson <56049905+chris-wjn@users.noreply.github.com>
2020-01-30 10:50:03 -0800

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,87 @@
+class Fraction {
+    private int numerator, denominator;
+    
+    public Fraction () {
+        numerator = 1;
+        denominator = 2;
+    }
+    public Fraction (int numerator, int denominator) {
+        numerator = numerator;
+        if (denominator == 0) denominator = 1;
+        else if (denominator != 0) denominator = denominator;
+    }
+    public Fraction (String fraction) {
+        numerator = Integer.parseInt(fraction.substring(0, 1));
+        int x = Integer.parseInt(fraction.substring(2, 3));
+        if (x == 0) denominator = 1;
+        else denominator = x;
+    }
+    public Fraction (Fraction fraction) {
+        numerator = fraction.numerator;
+        denominator = fraction.denominator;
+    }
+    public int getNum() {
+        return this.numerator;
+    }
+    public int getDenom() {
+        return this.denominator;
+    }
+    public String toString (Fraction fraction) {
+        String returnString = new String();
+        returnString = returnString + fraction.getNum() + "/" + fraction.getDenom();
+        return returnString;
+    }
+    public double toDouble (Fraction fraction) {
+        double i = (double) fraction.getNum();
+        double i2 = (double) fraction.getDenom();
+        return (i/i2);
+    }
+   public void reduce () {
+       int biggest = 0;
+       if (this.numerator > this.denominator) biggest = this.numerator;
+       else if (this.denominator > this.numerator) biggest = this.denominator;
+       else if (this.denominator == this.numerator) biggest = this.numerator;
+        for (int i = biggest; i >= 2; i--) {
+            if (Math.abs(this.numerator) % i == 0 && Math.abs(this.denominator) % i == 0) {
+                this.numerator = this.numerator/i;
+                this.denominator = this.denominator/i;
+            }
+        }
+   }
+   public void setNum (int numerator) {
+       this.numerator = numerator;
+   }
+   public void setDenom (int denominator) {
+       this.denominator = denominator;
+   }
+   public static Fraction multiply (Fraction a, Fraction b) {
+       Fraction finalFraction = new Fraction((a.getNum()*b.getNum()), (a.getDenom()*b.getDenom()));
+       finalFraction.reduce();
+       return finalFraction;
+    }
+    public static Fraction divide (Fraction a, Fraction b) {
+       Fraction finalFraction = new Fraction((a.getNum()*b.getDenom()), (a.getDenom()*b.getNum()));
+       finalFraction.reduce();
+       return finalFraction;
+    }
+    public static Fraction add (Fraction a, Fraction b) {
+       int an = a.getNum(), ad = a.getDenom(), bn = b.getNum(), bd = b.getDenom();
+       a.setNum(an * bd);
+       a.setDenom(ad * bd);
+       b.setNum(bn * ad);
+       b.setDenom(bd * ad);
+       Fraction finalFraction = new Fraction((a.getNum() + b.getNum()), a.getDenom());
+       finalFraction.reduce();
+       return finalFraction;
+    }
+    public static Fraction subtract (Fraction a, Fraction b) {
+       int an = a.getNum(), ad = a.getDenom(), bn = b.getNum(), bd = b.getDenom();
+       a.setNum(an * bd);
+       a.setDenom(ad * bd);
+       b.setNum(bn * ad);
+       b.setDenom(bd * ad);
+       Fraction finalFraction = new Fraction((a.getNum() - b.getNum()), a.getDenom());
+       finalFraction.reduce();
+       return finalFraction;
+    }
+}    
---
+++ FractionTest.java
@@ -0,0 +1,26 @@
+public class FractionTest {
+    public static void multTest() {
+        Fraction f = new Fraction(4, 5);
+        Fraction f2 = new Fraction(5, 4);
+        Fraction f3 = Fraction.multiply(f, f2);
+        System.out.println(f3.getNum() + "/" + f3.getDenom());
+    }
+    public static void divTest() {
+        Fraction f = new Fraction(4, 1);
+        Fraction f2 = new Fraction(1, 2);
+        Fraction f3 = Fraction.divide(f, f2);
+        System.out.println(f3.getNum() + "/" + f3.getDenom());
+    }
+    public static void addTest() {
+        Fraction f = new Fraction(-10, 2);
+        Fraction f2 = new Fraction(-8, 4);
+        Fraction f3 = Fraction.add(f, f2);
+        System.out.println(f3.getNum() + "/" + f3.getDenom());
+    }
+    public static void subTest() {
+        Fraction f = new Fraction(10, 3);
+        Fraction f2 = new Fraction(2, 6);
+        Fraction f3 = Fraction.subtract(f, f2);
+        System.out.println(f3.getNum() + "/" + f3.getDenom());
+    }
+}
